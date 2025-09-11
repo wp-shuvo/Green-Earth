916d6e6529e0f5c75cb29712e340c4a37ee40b5b
wp-shuvo <shuvoalshaied@gmail.com>
2025-09-11 21:24:00 +0600

    added a function

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("conected");
+
+const loadCategories = () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+
+  fetch(url)
+    .then((res) => res.json())
+    .then(data => console.log(data));
+};
+
+
