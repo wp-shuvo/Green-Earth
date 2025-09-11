f033394a2cfbf208f13debec1f83afbbda0ba7f1
wp-shuvo <shuvoalshaied@gmail.com>
2025-09-12 00:45:28 +0600

    added the categories

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-console.log("conected");
+// console.log("connected");
+
+//Categories
 
 const loadCategories = () => {
   const url = "https://openapi.programming-hero.com/api/categories";
 
   fetch(url)
     .then((res) => res.json())
-    .then(data => console.log(data));
+    .then(data => displayCategories(data?.categories));
 };
 
+const displayCategories = (categories) => {
+  console.log('from display -> ', categories);
 
+  const categoriesContainer = document.getElementById("add-categories");
+  categoriesContainer.innerHTML = "";
+
+  categories?.forEach(categorie => {
+    console.log('from forEach', categorie);
+
+    const createcatg = document.createElement("span");
+    createcatg.innerHTML = `
+      <span class="font-medium p-3 rounded-xl max-w-[250px] block hover:bg-[#15803d] hover:text-white">
+        ${categorie.category_name} 
+      </span>
+    `;
+
+    categoriesContainer.appendChild(createcatg);
+  });
+};
+
+loadCategories();
